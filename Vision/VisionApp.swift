e030424109bee0321ff2b1ae46b9b2d673d26e56
A-wakil <abdulwakil.ola@gmail.com>
2025-03-12 11:26:59 -0500

    Initial Commit

---
+++ Vision/VisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionApp.swift
+//  Vision
+//
+//  Created by CAIT on 3/12/25.
+//
+
+import SwiftUI
+
+@main
+struct VisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

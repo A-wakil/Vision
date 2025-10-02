fac97da443e7fe373af1bf8a65dc1a6a0e4b9e9f
Admin <admin@Admins-MacBook-Pro-A-wakil.local>
2025-10-02 11:02:03 -0500

    new branch

---
+++ Vision/OpenAIConfig.swift
@@ -0,0 +1,14 @@
+//
+//  OpenAIConfig.swift
+//  V-Eyes
+//
+//  Created by Admin on 9/29/25.
+//
+
+import Foundation
+
+struct OpenAIConfig {
+    static let apiKey = "sk-proj....."
+    static let baseURL = "https://api.openai.com/v1"
+}
+
